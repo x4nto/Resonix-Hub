667877e390ce126689ef21d7923f8cbcd83c7934
xanto <129802184+x4nto@users.noreply.github.com>
2025-03-09 21:24:17 +0100

    Update Rayfield.lua

--- Rayfield.lua
+++ Rayfield.lua
@@ -24,18 +24,6 @@ local Release = "Build 1.672"
 local RayfieldFolder = "Rayfield"
 local ConfigurationFolder = RayfieldFolder.."/Configurations"
 local ConfigurationExtension = ".rfld"
-local settingsTable = {
-	General = {
-		-- if needs be in order just make getSetting(name)
-		rayfieldOpen = {Type = 'bind', Value = 'K', Name = 'Rayfield Keybind'},
-		-- buildwarnings
-		-- rayfieldprompts
-
-	},
-	System = {
-		usageAnalytics = {Type = 'toggle', Value = true, Name = 'Anonymised Analytics'},
-	}
-}
 
 local HttpService = getService('HttpService')
 local RunService = getService('RunService')
